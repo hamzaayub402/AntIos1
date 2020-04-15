0443d6064cfeb4893e2ed42c0f20be049e03a444
Egenie Next <egenie.apple@gmail.com>
2020-04-15 14:32:10 +0500

    test methods

--- ios/AntIos1.m
+++ ios/AntIos1.m
@@ -4,8 +4,15 @@
 @implementation AntIos1
 
 RCT_EXPORT_MODULE()
-
+//RCT_EXTERN_MODULE(LiveStream, NSObject)
+RCT_EXPORT_METHOD(turnOn)
 RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+{
+    // TODO: Implement some actually useful functionality
+    callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
+}
+
+RCT_EXPORT_METHOD(sampleMethod1:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
 {
     // TODO: Implement some actually useful functionality
     callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@", numberArgument, stringArgument]]);
